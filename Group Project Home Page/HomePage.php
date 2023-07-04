6bff0e64bed0f28e0d83dae62baf9edf0cfb4eea
Ben McCloskey <88287124+Monotone712@users.noreply.github.com>
2023-07-04 22:03:30 +0900

    Add files via upload

---
+++ Group Project Home Page/HomePage.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Biomat USA</title>
+  <link rel="stylesheet" type="text/css" href="styleshomepage.css">
+</head>
+<body>
+  <div class="Frame2">
+    <div class="Rectangle1"></div>
+    <div class="Frame22">
+      <div class="Donors">Donors</div>
+      <div class="Appointments">Appointments</div>
+      <div class="Locations">Locations</div>
+      <div class="Frame42">
+        <a href="/Group Project Sign In/signin.php"<div class="SignIn" style="color: #3d5656; text-decoration: none;">Sign In</div></a>
+      </div>
+    </div>
+    <a href="/Group Project Home Page/HomePage.php" style="text-decoration: none;"><div class="BiomatUsa">BIOMAT <br>USA</div></a>
+    <div class="Rectangle9"></div>
+    <div class="Rectangle10"></div>
+    <div class="ImprovingLivesThroughPlasma">Improving lives through plasma.</div>
+    <div class="Subheader1">Join the thousands of donors making the difficult choice to give to hundreds of others.</div>
+    <div class="Frame23">
+      <div class="GetStarted">Get Started</div>
+      <div class="Group14">
+        <div class="Vector"></div>
+        <div class="Vector"></div>
+      </div>
+    </div>
+    <div class="WhyChooseBiomat">Why Choose Biomat?</div>
+    <div class="Rectangle11"></div>
+    <div class="Rectangle12"></div>
+    <div class="Rectangle13"></div>
+    <div class="LoremIpsum1">Lorem Ipsum</div>
+    <div class="LoremIpsum2">Lorem Ipsum</div>
+    <div class="LoremIpsum3">Lorem Ipsum</div>
+    <div class="LoremIpsumPara1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+    <div class="LoremIpsumPara2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+    <div class="LoremIpsumPara3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+  </div>
+</body>
+</html>
---
+++ Group Project Sign In/SignIn.php
@@ -0,0 +1,30 @@
+<html>
+
+	<head>
+		<title>BiomatUSA Sign In</title>
+		<link rel="stylesheet" type="text/css" href="stylessignin.css">
+	<head>
+	
+	<body>
+		<div class="Rectangle1"></div>
+		<div class="Frame22">
+			<div class="Donors">Donors</div>
+			<div class="Appointments">Appointments</div>
+			<div class="Locations">Locations</div>
+      	<div class="Frame42">
+      		<a href="/Group Project Sign In/signin.php"<div class="SignIn" style="color: #3d5656; text-decoration: none;">Sign In</div></a>
+      	</div>
+      	</div>
+      	<a href="/Group Project Home Page/HomePage.php" style="text-decoration: none;"><div class="BiomatUsa">BIOMAT <br>USA</div></a>
+      	<div class="SignInBig">Sign In</div>
+     
+      	<div class="Form">
+      	<form method="post" action="homepage.php">
+      		<input type="text" placeholder="Username" required>
+     		<input type="password" placeholder="Password" required>
+      		<input type="submit" value="Login">
+      	</form>
+      	</div>
+	</body>
+
+</html>
