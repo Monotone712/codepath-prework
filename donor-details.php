a8369b7d9ecffe50cffefdec8b9edece592f44b4
keacart <138012491+keacart@users.noreply.github.com>
2023-07-04 23:39:37 +0900

    Add files via upload

---
+++ donor-details.php
@@ -0,0 +1,65 @@
+<head>
+	<link rel="stylesheet" href="Styledd.css">
+	<title>Donors</title>
+</head>
+<body>
+	<div id = "top_banner">
+		<button>User</button>
+	</div>
+	<div id ="section_2">	
+		<nav>
+			<ul id = "nav_column">
+				<li><a href="HomePage.php">Home</a></li>
+				<li><a href="donor-list.php">Donors</a></li>
+			</ul>
+		</nav>
+        <div id="dd_box">
+            <div id="title">Donor Details</div>
+            <center><button id="edit">Edit</button></center>
+            <div id ="ddi_box"></div>
+            <div class = "table">
+                <table>
+                    <tr>
+                        <th>
+                            Name
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>
+                            Keaton Carter
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Email
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>
+                            U343434@utah.edu
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Phone
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>
+                            801-979-2343
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Address
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>
+                            1234 East Purple Block Lane, Salt Lake City, UT, 84030
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
---
+++ donor-list.php
@@ -0,0 +1,165 @@
+<html>
+<head>
+	<link rel="stylesheet" href="Style.css">
+	<title>Donors</title>
+</head>
+<body>
+	<div id = "top_banner">
+		<button>User</button>
+	</div>
+	<div id ="navbar_table">	
+		<nav>
+			<ul id = "nav_column">
+				<li><a href="HomePage.php">Home</a></li>
+				<li><a href="donor-list.php">Donors</a></li>
+			</ul>
+		</nav>
+		<div id = "searchbar_adddonor">
+			<form class = "form1">
+				<input type="text" id="search" name="search" placeholder="Search">
+				<input type="submit" value="Search">
+			</form>
+			<button class = "add_donor">Add Donor</button>
+		</div>
+		<div id = "donor_table">
+			<table>
+				<tr>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Phone</th>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+				<tr>
+					<td>Keaton Carter</td>
+					<td>u343434@utah.edu</td>
+					<td>801-979-2499</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
